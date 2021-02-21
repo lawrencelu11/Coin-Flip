c2f177327dc6613ee86f0e150c6a891729473eaa
Lawrence Lu <lawrencelu11@yahoo.com>
2021-02-20 20:24:10 -0800

    Add files via upload

---
+++ CoinFlipper.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void FlipCoin()
+{
+    srand(time(0));
+    int counter, hcount, tcount = 0;
+    int nflip, outcome;
+
+    cout << "Enter number of coin flips: ";
+    cin >> nflip;
+    cout << "\n";
+    while(counter < nflip)
+    {
+        outcome = rand() % 3 + 1;
+        if(outcome == 1)
+        {
+            cout << "Heads\n";
+            hcount++;
+        }else{
+            cout << "Tails\n";
+            tcount++;
+        }
+        counter++;
+    }
+    cout << "\n";
+    cout << "Heads: " << hcount << endl;
+    cout << "Tails: " << tcount << endl;
+    cout << "Number of flips: " << hcount + tcount << endl;
+}
+
+int main()
+{
+    FlipCoin();
+}
+
+
+
+
+
+
